225081b2ca9f464d28f1e815e82f9fbd7ad65f10
Lang <gilangwasi32@gmail.com>
2026-05-12 15:48:44 +0700

    chore: remove .next from git tracking

--- .next/dev/static/chunks/src_app_page_tsx_03w.znv._.js
+++
@@ -1,5 +0,0 @@
-(globalThis["TURBOPACK_CHUNK_LISTS"] || (globalThis["TURBOPACK_CHUNK_LISTS"] = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: ["static/chunks/src_components_ParticleField_tsx_06_fek.._.js","static/chunks/src_0zuj5.9._.js","static/chunks/node_modules_next_0jtz4ss._.js","static/chunks/src_063npa~._.css"],
-    source: "dynamic"
-});
